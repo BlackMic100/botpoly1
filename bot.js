92529823d5ab0fd2c5229ea928a685e2fdef690d
BlackMic100 <37043307+BlackMic100@users.noreply.github.com>
2018-09-18 16:03:33 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -248,7 +248,7 @@ message.channel.sendEmbed(cat);
  
 const moment = require('moment');
              client.on('message', message => {
-                 var prefix = ""
+                 var prefix = "$"
            if (message.content.startsWith(prefix + "id")) {
      var args = message.content.split(" ").slice(1);
      let user = message.mentions.users.first();
