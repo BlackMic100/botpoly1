b94894f8d84871a4fcbc5127a822fe3580e7e108
BlackMic100 <37043307+BlackMic100@users.noreply.github.com>
2018-09-21 23:41:45 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -407,6 +407,28 @@ if (message.content.startsWith('maelumh')) {
 
 
 
+client.on('message',async msg => {
+  var p = "";
+  if(msg.content.startsWith(p + "setdate")) {
+  if(!msg.guild.member(msg.author).hasPermissions('MANAGE_CHANNELS')) return msg.reply('❌ **go play minecraft**');
+  if(!msg.guild.member(client.user).hasPermissions(['MANAGE_CHANNELS'])) return msg.reply('❌ **البوت لا يمتلك صلاحية**');
+  msg.guild.createChannel(`date :[]` , 'voice').then(time => {
+    time.overwritePermissions(msg.guild.id, {
+      CONNECT: false,
+      SPEAK: false
+    });
+  setInterval(() => {
+      var currentTime = new Date(),
+Year = currentTime.getFullYear(),
+Month = currentTime.getMonth() + 1,
+Dat = currentTime.getDate()
+      time.setName(`Date : [${Year} - ${Month} - ${Dat}]]`);
+ },1000);
+  });
+  }
+ 
+});
+
 
 
 
