25c80fc674aed1115220a2992c174017129e2815
BlackMic100 <37043307+BlackMic100@users.noreply.github.com>
2018-09-21 23:46:25 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -408,27 +408,64 @@ if (message.content.startsWith('maelumh')) {
 
 
 client.on('message',async msg => {
-  var p = "";
-  if(msg.content.startsWith(p + "setdate")) {
+     if(msg.channel.type === "dm") return;
+  if(msg.author.bot) return;
+  var p = "*";
+  if(msg.content.startsWith(p + "setstats")) {
   if(!msg.guild.member(msg.author).hasPermissions('MANAGE_CHANNELS')) return msg.reply('❌ **go play minecraft**');
   if(!msg.guild.member(client.user).hasPermissions(['MANAGE_CHANNELS'])) return msg.reply('❌ **البوت لا يمتلك صلاحية**');
-  msg.guild.createChannel(`date :[]` , 'voice').then(time => {
-    time.overwritePermissions(msg.guild.id, {
+  var ggg= msg.guild.createChannel('SERVER STATS', 'category').then(kk => {
+           var ccc =msg.guild.createChannel('SERVER STATS', 'voice').then(al => {
+                var aa =msg.guild.createChannel('SERVER STATS', 'voice').then(alp => {
+                   var aaa =msg.guild.createChannel('SERVER STATS', 'voice').then(alph => {
+       al.setParent(kk);
+       alp.setParent(kk);
+       alph.setParent(kk);
+       
+     al.overwritePermissions(msg.guild.id, {
+      CONNECT: false,
+      SPEAK: false
+    });
+     alp.overwritePermissions(msg.guild.id, {
+      CONNECT: false,
+      SPEAK: false
+    });
+     alph.overwritePermissions(msg.guild.id, {
       CONNECT: false,
       SPEAK: false
     });
   setInterval(() => {
       var currentTime = new Date(),
+hours = currentTime.getHours() + 3 ,
+minutes = currentTime.getMinutes(),
+Seconds = currentTime.getSeconds(),
 Year = currentTime.getFullYear(),
 Month = currentTime.getMonth() + 1,
 Dat = currentTime.getDate()
-      time.setName(`Date : [${Year} - ${Month} - ${Dat}]]`);
+if (minutes < 10) {
+minutes = "0" + minutes;
+}
+var suffix = "AM";
+if (hours >= 12) {
+suffix = "PM";
+hours = hours - 12;
+}
+if (hours == 0) {
+hours = 12;
+}
+     al.setName(`Voice Online :[ ${msg.guild.members.filter(m => m.voiceChannel).size} ]`);
+      alp.setName(`Time :[${hours} : ${minutes} : ${Seconds} ${suffix}]`);
+        alph.setName(`[ Date : [${Year} - ${Month} - ${Dat} ]`);
  },1000);
-  });
+                   })
+    
+                })
+           })
+  })
+           
   }
  
 });
-
 
 
 
