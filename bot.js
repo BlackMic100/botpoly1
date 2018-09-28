0d951af634f377ad3e9c62989b1ad80f858e1e25
BlackMic100 <37043307+BlackMic100@users.noreply.github.com>
2018-09-28 10:52:15 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -949,7 +949,19 @@ message.channel.sendFile(canvas.toBuffer())
 
 
 
+client.on('message', async msg => {
+     client.snek = require('snekfetch');
+    var p = ""
+  if(msg.content.startsWith(p + "cmind")) {
+   let args = msg.content.split(' ').slice(1).join(' ');
 
+ if(args.length < 1) return args.missing(msg, 'No text added', this.help);
+  msg.channel.startTyping();
+  const searchMessage = await msg.channel.send('🖌️Painting...');
+  const { body } = await client.snek.get(`https://nekobot.xyz/api/imagegen?type=changemymind&text=${encodeURIComponent(args)}`);
+  msg.channel.send({file: { attachment:body.message, name: 'changemymind.png'}}).then(()=> { searchMessage.delete(); msg.channel.stopTyping(); });
+};
+});
 
 
 
