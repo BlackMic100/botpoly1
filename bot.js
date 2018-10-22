d77e8016b4b0eeb7a466bc6939cd7b648fe0f22f
BlackMic100 <37043307+BlackMic100@users.noreply.github.com>
2018-10-22 19:56:55 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -59,7 +59,22 @@ client.on('message', msg => {//msg
 
 
 
+  client.on('message', message => { 
+    var prefix = "-";
+ let args = message.content.split(' ').slice(1);
+    if(message.content.startsWith(prefix + 'اختصار')) {
+    if(!message.channel.guild) return;  
 
+        googl.setKey('AIzaSyC2Z2mZ_nZTcSvh3QvIyrmOIFP6Ra6co6w');
+        googl.getKey();
+        googl.shorten(args.join(' ')).then(shorturl => {
+            message.channel.send(''+shorturl)
+        }).catch(e=>{
+            console.log(e.message);
+            message.channel.send('خطأ!');
+        });
+}
+});
 
 
   client.on("message", msg => {
