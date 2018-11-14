48e9880a489a6105649409b49240e942a0f5c9b0
BlackMic100 <37043307+BlackMic100@users.noreply.github.com>
2018-11-14 19:01:51 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -164,7 +164,32 @@ hours = 12;
 
 
 
+client.on('message', function(message) {//by turkyKSA12
+    if(!message.channel.guild) return;
+if(message.content ===  '$color 140') {
+if(message.member.hasPermission('MANAGE_ROLES')) {
+setInterval(function(){})
+message.channel.send('جاري عمل الالوان |✅')
+}else{
+message.channel.send('ما معاك البرمشن المطلوب  |❌')
+}
+}
+});
 
+client.on('message', message=>{
+if (message.content ===  '$color 140'){
+if(!message.channel.guild) return;
+if (message.member.hasPermission('MANAGE_ROLES')){
+  setInterval(function(){})
+    let count = 0;
+    let ecount = 0;
+for(let x = 1; x < 141; x++){
+message.guild.createRole({name:x,
+color: 'RANDOM'})
+}
+}
+}
+});
 
 
 
