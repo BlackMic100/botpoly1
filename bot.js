1527d6e4e4d9bfa776d8f89a17fecdbf83aad561
BlackMic100 <37043307+BlackMic100@users.noreply.github.com>
2018-11-19 14:59:30 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -34,6 +34,31 @@ client.on('message', message => {
 
 
 
+
+
+
+
+
+client.on('message', message => {
+  
+    if(message.content.split(' ')[0] == 'مقترح'){
+         if(!message.channel.guild) return;
+                            let args = message.content.split(' ').slice(1).join(' ');
+    
+    client.guilds.get("445270097993662474").members.get("257111476404224001").sendMessage(message.author.tag+"\n Message : "+args)
+    
+                                                    let embed = new Discord.RichEmbed()
+                                                    .setAuthor(message.author.username, message.author.avatarURL)
+                                                    .setDescription(':mailbox_with_mail: تم ارسال صاحب البوت بنجاح')
+                                                    .setThumbnail(message.author.avatarURL)
+                                                    .setFooter(message.author.username, message.author.avatarURL)
+                                                    message.channel.sendEmbed(embed);}
+                                                  });
+
+
+
+
+
 client.on('ready', () => {
      client.user.setActivity("العيد الوطني 48 المجيد . اوامر",{type: 'WATCHING'})
 
